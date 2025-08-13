bdb10816fd3301c7b2b7fc1b4e6a294271f15403
BjornJohanIS <142797702+BjornJohanIS@users.noreply.github.com>
2025-08-13 16:37:52 +0200

    Update LaunchReviewPlugin.java

--- android/src/main/java/com/iyaffle/launchreview/LaunchReviewPlugin.java
+++ android/src/main/java/com/iyaffle/launchreview/LaunchReviewPlugin.java
@@ -1,124 +1,41 @@
 package com.iyaffle.launchreview;
+
 import android.widget.Toast;
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.PluginRegistry;
-
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
-import 	android.content.pm.ActivityInfo;
 
 import androidx.annotation.NonNull;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
 import java.util.List;
-/**
- * LaunchReviewPlugin
- */
-public class LaunchReviewPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
-    Activity activity;
 
-    private static LaunchReviewPlugin register(LaunchReviewPlugin plugin, BinaryMessenger messenger, Activity activity) {
-        final MethodChannel channel = new MethodChannel(messenger, "launch_review");
-        plugin.activity = activity;
-        channel.setMethodCallHandler(plugin);
-        return plugin;
-    }
-
-    /**
-     * Plugin registration.
-     */
-    @SuppressWarnings("deprecation")
-    public static void registerWith(PluginRegistry.Registrar registrar) {
-        register(new LaunchReviewPlugin(), registrar.messenger(), registrar.activity());
-    }
-
-    @Override
-    public void onMethodCall(MethodCall call, Result result) {
-        if (call.method.equals("launch")) {
-            String appId = call.argument("android_id");
-            String toastMessage = call.argument("toast_message");
-            boolean showToast = call.argument("show_toast");
-
-            if (appId == null) {
-                appId = activity.getPackageName();
-            }
-
-            if (toastMessage == null){
-                toastMessage = "Please Rate Application";
-            }
-            Intent rateIntent = new Intent(Intent.ACTION_VIEW,
-                    Uri.parse("market://details?id=" + appId));
-            boolean marketFound = false;
-
-            // find all applications able to handle our rateIntent
-            final List<ResolveInfo> otherApps =  activity.getPackageManager()
-                    .queryIntentActivities(rateIntent, 0);
-            for (ResolveInfo otherApp: otherApps) {
-                // look for Google Play application
-                if (otherApp.activityInfo.applicationInfo.packageName
-                        .equals("com.android.vending")) {
-
-                    ActivityInfo otherAppActivity = otherApp.activityInfo;
-                    ComponentName componentName = new ComponentName(
-                            otherAppActivity.applicationInfo.packageName,
-                            otherAppActivity.name
-                    );
-                    // make sure it does NOT open in the stack of your activity
-                    rateIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    // task reparenting if needed
-                    rateIntent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-                    // if the Google Play was already open in a search result
-                    //  this make sure it still go to the app page you requested
-                    rateIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    // this make sure only the Google Play app is allowed to
-                    // intercept the intent
-                    rateIntent.setComponent(componentName);
-                   if (showToast){
-                        Toast.makeText(activity, toastMessage, Toast.LENGTH_SHORT).show();
-                   } 
-
-                    activity.startActivity(rateIntent);
-                    marketFound = true;
-                    break;
-
-                }
-            }
-
-            // if GP not present on device, open web browser
-            if (!marketFound) {
-                try {
-                    activity.startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("market://details?id=" + appId)));
-                } catch (ActivityNotFoundException e) {
-                    activity.startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("https://play.google.com/store/apps/details?id=" + appId)));
-                }
-            }
-            result.success(null);
-        }  else {
-            result.notImplemented();
-        }
-    }
+public class LaunchReviewPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler {
+    private Activity activity;
+    private MethodChannel channel;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-        register(this, binding.getBinaryMessenger(), null);
+        channel = new MethodChannel(binding.getBinaryMessenger(), "launch_review");
+        channel.setMethodCallHandler(this);
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-
+        channel.setMethodCallHandler(null);
+        channel = null;
     }
 
     @Override
@@ -128,16 +45,81 @@ public class LaunchReviewPlugin implements MethodCallHandler, FlutterPlugin, Act
 
     @Override
     public void onDetachedFromActivityForConfigChanges() {
-
+        activity = null;
     }
 
     @Override
     public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-
+        activity = binding.getActivity();
     }
 
     @Override
     public void onDetachedFromActivity() {
+        activity = null;
+    }
 
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("launch")) {
+            if (activity == null) {
+                result.error("NO_ACTIVITY", "LaunchReviewPlugin: activity is null", null);
+                return;
+            }
+
+            String appId = call.argument("android_id");
+            String toastMessage = call.argument("toast_message");
+            boolean showToast = call.argument("show_toast");
+
+            if (appId == null) {
+                appId = activity.getPackageName();
+            }
+
+            if (toastMessage == null) {
+                toastMessage = "Please Rate Application";
+            }
+
+            Intent rateIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + appId));
+            boolean marketFound = false;
+
+            List<ResolveInfo> otherApps = activity.getPackageManager()
+                    .queryIntentActivities(rateIntent, 0);
+            for (ResolveInfo otherApp : otherApps) {
+                if (otherApp.activityInfo.applicationInfo.packageName.equals("com.android.vending")) {
+                    ActivityInfo otherAppActivity = otherApp.activityInfo;
+                    ComponentName componentName = new ComponentName(
+                            otherAppActivity.applicationInfo.packageName,
+                            otherAppActivity.name
+                    );
+
+                    rateIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED |
+                            Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    rateIntent.setComponent(componentName);
+
+                    if (showToast) {
+                        Toast.makeText(activity, toastMessage, Toast.LENGTH_SHORT).show();
+                    }
+
+                    activity.startActivity(rateIntent);
+                    marketFound = true;
+                    break;
+                }
+            }
+
+            if (!marketFound) {
+                try {
+                    activity.startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("market://details?id=" + appId)));
+                } catch (ActivityNotFoundException e) {
+                    activity.startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://play.google.com/store/apps/details?id=" + appId)));
+                }
+            }
+
+            result.success(null);
+        } else {
+            result.notImplemented();
+        }
     }
 }
